4939e9a82481aa42c0aae76eca9f3a4d61965e5c
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-11-30 14:12:26 +0530

    file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
+import TravelCards from "./Travelcards"
 function App(){
   return(
   
     <>
-    <h1 className="text-5xl bg-blue-500 px-5 py-6 m-9 text-center text-amber-50">hii sneha</h1>
-    <button className="border-2 p-3 rounded-2xl text-center text-3xl text-blue-500">SUBBMIT</button>
-    <p className="text-4xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, quibusdam itaque! Aut exercitationem amet ipsa. Harum deserunt nulla facere esse veniam molestias illo earum quam blanditiis mollitia, beatae tenetur dignissimos?</p>
-    <section className="flex justify-around p-10 bg-amber-400 text-3xl">
-    <h1>my name sneha tiwari</h1>
-    <h1>my name sneha tiwari</h1>
-    <h1>my name sneha tiwari</h1>
-    <h1>my name sneha tiwari</h1>
-    </section>
+    
+  
+    <TravelCards/>
     </>
   )
 }
